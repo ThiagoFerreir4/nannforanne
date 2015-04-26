979cfb0128ed439f3e1f2bc13cb2aaa4d7acc9f8
Bianca Mihai <bubu.slubu@gmail.com>
2015-04-26 14:57:57 +0300

    create api server

---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+// Nann for Anne main 
+// Author Bianca Mihai <bubu.slubu@gmail.com>
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:3000/v1)
+// will add here controllers
+router.get('/', function(req, res) {
+    res.json({ message: 'welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/v1', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('App started on: ' + port);
